b1ee14f06dfae631074e5351916c1c4939f92b71
aAlvarez03 <aalvarez01@informatica.iesvalledeljerteplasencia.es>
2024-10-08 18:51:01 +0200

    Creacion de los partials

---
+++ views/partials/fin-doc.part.php
@@ -0,0 +1,11 @@
+   <!-- Jquery -->
+   <script type="text/javascript" src="js/jquery.min.js"></script>
+   <!-- Bootstrap core Javascript -->
+   <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+   <!-- Plugins -->
+   <script type="text/javascript" src="js/jquery.easing.min.js"></script>
+   <script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+   <script type="text/javascript" src="js/scrollreveal.min.js"></script>
+   <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
--- views/single_post.view.php
+++ views/single_post.view.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	  <title>PhotographItem-Responsive Theme | Blog</title>
-
-  	<!-- Bootstrap core css -->
-  	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
-  	<!-- Bootstrap core css -->
-  	<link rel="stylesheet" type="text/css" href="css/style.css">
-  	<!-- Font Awesome icons -->
-  	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-<body id="page-top">
+<?php include __DIR__.'/partials/inicio-doc.part.php' ?>
 
 <!-- Navigation Bar -->
    <nav class="navbar navbar-fixed-top navbar-default">
@@ -186,14 +164,4 @@
      </div>
    </footer>
 
-<!-- Jquery -->
-   <script type="text/javascript" src="js/jquery.min.js"></script>
-   <!-- Bootstrap core Javascript -->
-   <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-   <!-- Plugins -->
-   <script type="text/javascript" src="js/jquery.easing.min.js"></script>
-   <script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
-   <script type="text/javascript" src="js/scrollreveal.min.js"></script>
-   <script type="text/javascript" src="js/script.js"></script>
-</body>
-</html>
+   <?php include __DIR__.'/partials/fin-doc.part.php' ?>
